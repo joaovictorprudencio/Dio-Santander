c0f9179ee8f7943bfcf0b024c68d623e2fd21159
joao victor <joaovprudencio13@gmail.com>
2024-05-09 17:40:49 -0300

    desafio do modulo de java basico

---
+++ src/ContaTerminal.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+   
+public static void main(String[] args) {
+    String Nome, Agencia;
+    int Saldo, Numero;
+
+
+    Scanner DadosCliente = new Scanner(System.in);
+
+    System.out.println("qual é seu nome? :   ");
+    Nome = DadosCliente.nextLine();
+
+    
+   System.out.println("digite numero de usaurio:  ");
+   Numero = DadosCliente.nextInt();
+
+   System.out.println("qual saldo inicial:  ");
+   Saldo = DadosCliente.nextInt();
+
+     DadosCliente.nextLine();
+    System.out.println("digite o numero da agencia:   ");
+    Agencia =  DadosCliente.nextLine();
+
+
+    System.out.println("Olá "+Nome+", Obrigado por criar sua conta em nosso banco, sua agencia é "+Agencia+ ", conta "+Numero+" e seu saldo inicial de "+Saldo+"$ Já está disponivel para saque");
+
+}
+    
+
+}
